95b904180734561816c03232dc92d3f705440b70
Pawel Goc <pawel.goc@solutionsby.pl>
2024-04-03 02:11:48 +0200

    edit handleChange and add error in TextField

--- src/components/inputWrapper/InputWrapper.tsx
+++ src/components/inputWrapper/InputWrapper.tsx
@@ -1,25 +1,33 @@
 import { Input } from "../input/Input"
 import { useData } from '../DataProvider/DataProvider';
+import { useState } from "react";
 
 
 export const InputWrapper = ()=>{
     const { setNumberOfRows,numberOfRows} = useData();
+    const [status , setStatus]=useState<string>('Podaj liczbe wierszy');
+    const [errorMSG, setErrorMsg]= useState<string>('');
     
     const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
         const parsedValuue = parseInt(e.target.value);
         if(!isNaN(parsedValuue) && setNumberOfRows){
-            if(parsedValuue <= 100){
-                setNumberOfRows(parsedValuue)
-            }else{
-                alert('Wartość nie moze przekraczć 100 wierszy na stronę ')
-            } 
+            checkValue(parsedValuue,setNumberOfRows)
         }
     }
 
+    const checkValue = (value,set)=>{
+        if(value <= 100){
+            set(value)
+        }else{
+            setStatus('Error');
+            setErrorMsg('Max 100');
+            setNumberOfRows(100);
+        } 
+    }
 
     return(
         <>   
-        <Input value={numberOfRows} label={"Podaj liczbe wierszy"} type={'number'} handle={handleChange}/>
+        <Input value={numberOfRows} label={status}type={'number'} handle={handleChange} error={errorMSG}/>
         
         </>
     )
